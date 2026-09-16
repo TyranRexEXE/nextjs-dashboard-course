490b07a479030ffa35332564e5ac55d5f5e44823
TyranRexEXE <stanislav.pohrebniakov@onix-systems.com>
2026-09-16 15:52:05 +0300

    fix searchParams

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -6,13 +6,14 @@ export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default async function Customers({ searchParams }: {
-  searchParams?: {
+export default async function Customers(props: {
+  searchParams?: Promise<{
     query?: string;
     page?: string;
-  };
+  }>;
 }) {
-  const query = searchParams?.query || '';
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || ''
   const customers = await fetchFilteredCustomers(query)
 
   return (
